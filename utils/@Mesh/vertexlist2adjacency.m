37975a92bad8cebc19c90bcb9781b07f06f3b143
Tingran Gao <gaotingran@gmail.com>
2016-11-02 19:58:02 -0400

    prepare for fixing typo in commit history (look too bad)

--- utils/@Mesh/vertexlist2adjacency.m
+++
@@ -1,7 +0,0 @@
-function A = vertexlist2adjacency(Vlist)
-n=numel(Vlist);
-A=zeros(n);
-for i=1:n
-    A(i,Vlist{i})=1;
-end
-
--- utils/conformal/TEETH_remove_nans.m
+++
@@ -1,8 +0,0 @@
-function [A] = TEETH_remove_nans(A)
-
-temp = A(1);
-while (isnan(temp))
-    A(1)=[];
-    temp = A(1);
-    
-end
