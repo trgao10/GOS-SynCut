6be48cdbeb1e0797fcd55dc180968f86c0aabb39
Rachel <ry27@duke.edu>
2016-11-07 08:36:52 -0500

    new func

---
+++ utils/randSO3.m
@@ -0,0 +1,22 @@
+function r = randSO3
+
+% sample random SO3 matrix with normal distribution on rotation angle theta
+
+% sample angle
+theta = rand;
+while abs(theta) > pi
+    theta = rand;
+end
+
+% sample rotation axis
+k = rand(3,1);
+k = k/norm(k);
+% generate anti-symmetric matrix K
+K = zeros(3);
+K(1,2) = k(1);
+K(1,3) = k(2);
+K(2,3) = k(3);
+K = K - K';
+
+% take matrix exponential
+r = expm(theta * K);
